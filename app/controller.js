bfc4d924aa78444227bb5c1ccf23ee5e4b1fe92b
Zajitarius <zajitariusa@gmail.com>
2015-08-03 01:47:04 +0700

    Page1 Patch1

--- app/controller.js
+++ app/controller.js
@@ -11,34 +11,80 @@
     .module('boilerplate')
     .controller('MainController', MainController);
 
-  MainController.$inject = ['LocalStorage', 'QueryService', '$scope','$http', '$parse','parallaxHelper'];
+  MainController.$inject = ['LocalStorage', 'QueryService', '$scope','$http', '$parse','parallaxHelper', '$filter'];
 
 
-  function MainController(LocalStorage, QueryService, $scope, $http, $parse, parallaxHelper) {
+  function MainController(LocalStorage, QueryService, $scope, $http, $parse, parallaxHelper, $filter) {
 
     // 'controller as' syntax
     var self = this;
 
-    $scope.username = "test";
-    $scope.result = "Ha Ha Ha";
-
     ////////////  function definitions
 
-    $scope.usernameChange = function() {
-      $scope.result = $scope.username + "Ha Ha Ha";
+    $http.get('/NewTravel.json').
+    then(function(response) {
+      $scope.travelData = response.data.cignatravel;
+      
+    }, function(response) {
+      
+    });
+
+    $scope.addDestinations = function() {
+      if($scope.travel.destination){
+        $scope.destinations.push($scope.travel.destination);
+        $scope.travelData.destination = $filter('filter')($scope.travelData.destination, {country: "!"+$scope.travel.destination.country}, true);
+        $scope.travel.destination = "";
+      }
+    }
+
+    $scope.removeDestination = function(index) {
+      console.log($scope.destinations[index]);
+      $scope.travelData.destination.push($scope.destinations[index]);
+      $scope.destinations = $filter('filter')($scope.destinations, {country: "!"+$scope.destinations[index].country}, true);
+    }
+
+    $scope.range = function(min, max, step){
+      step = step || 1;
+      var input = [];
+      for (var i = min; i <= max; i += step) input.push(i);
+      return input;
     };
 
-    $scope.background = parallaxHelper.createAnimator(-0.5, 1000, -150);
+    $scope.addDays = function(date, days) {
+      var result = new Date(date);
+      result.setDate(result.getDate() + days);
+      return result;
+    }
 
+    $scope.resetEndDate = function() {
+      $scope.travel.endDate = null;
+      $scope.travel.days = "";
+    }
 
-    /**
-     * Load some data
-     * @return {Object} Returned object
-     */
-    // QueryService.query('GET', 'posts', {}, {})
-    //   .then(function(ovocie) {
-    //     self.ovocie = ovocie.data;
-    //   });
+    $scope.calcTravelDays = function() {
+      var oneDay = 24*60*60*1000;
+      $scope.travel.days = "รวม "+(Math.floor(( Date.parse($scope.travel.endDate) - Date.parse($scope.travel.startDate) ) / oneDay)+1)+" วัน";
+    }
+
+    $scope.destinations = [];
+    $scope.travel = [];
+    $scope.travel.startDate = new Date();
+    $scope.maxDate = $scope.addDays($scope.travel.startDate,90);
+    
+
+    $scope.dateOptions = {
+      formatYear: 'yy',
+      startingDay: 1
+    };
+
+    $scope.today = new Date();
+
+    $scope.minDate = $scope.addDays($scope.travel.startDate,1);
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
+
+    $scope.background = parallaxHelper.createAnimator(-0.05);
   }
 
 
