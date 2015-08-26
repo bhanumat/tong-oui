cd8c9cf441e47a807afeb26d1adb2a811871cf5b
Sarat Rachatavet <ct.sarat.r@gmail.com>
2015-08-26 07:05:44 -0400

    add function to select a plan

--- js/scripts.js
+++ js/scripts.js
@@ -10,12 +10,18 @@
     jQuery(document).ready(function($){
 		$(this).on('mouseover','#plan-main',function(){
 			$('#plan-main-scroll').on('scroll',function(){
-				$('#plan-options-scroll').scrollLeft($(this).scrollLeft());
+				$('#plan-options1-scroll').scrollLeft($(this).scrollLeft());
+				$('#plan-options2-scroll').scrollLeft($(this).scrollLeft());
 			});
 		});
 		$(this).on('mouseover','#plan-options',function(){
-			$('#plan-options-scroll').on('scroll',function(){
+			$('#plan-options1-scroll').on('scroll',function(){
 				$('#plan-main-scroll').scrollLeft($(this).scrollLeft());
+				$('#plan-options2-scroll').scrollLeft($(this).scrollLeft());
+			});
+			$('#plan-options2-scroll').on('scroll',function(){
+				$('#plan-main-scroll').scrollLeft($(this).scrollLeft());
+				$('#plan-options1-scroll').scrollLeft($(this).scrollLeft());
 			});
 		});
 
